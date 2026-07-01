13c115c342e4807ccf6dbaecd50f50745cc1c165
vasahil15 <varmasahil877@gmail>
2026-07-01 14:11:24 +0530

    added backend link in the frontend

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
-    withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL || 'https://dbloginbackend.vercel.app',
+  withCredentials: true,
 });
+
 export default API;
